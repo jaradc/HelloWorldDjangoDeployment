ef788e05d4d30c9013ee3dab53577c41d8fec8e1
Jarad Collier <jnscollier@gmail.com>
2021-04-14 20:04:46 -0700

    Changed uploads ordering

--- uploads/models.py
+++ uploads/models.py
@@ -11,5 +11,8 @@ class FileUpload(models.Model):
     def __str__(self):
         return self.upload.name
 
+    class Meta:
+        ordering = ['-upload_at']
+
 # TODO: https://docs.djangoproject.com/en/3.0/ref/signals/#django.db.models.signals.pre_delete
 # Add pre_delete or post_delete
